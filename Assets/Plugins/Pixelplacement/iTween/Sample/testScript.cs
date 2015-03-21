15d094fbb0ec410678a77e3dc113e67b3ec6631a
yoshimune <seatangle.dash@gmail.com>
2015-03-21 15:40:05 +0900

    add nozomi model ;

---
+++ Assets/Plugins/Pixelplacement/iTween/Sample/testScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class testScript : MonoBehaviour {
+
+
+     public TextMesh textMesh;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetMouseButtonDown (0)) {
+            iTween.ValueTo (this.gameObject, iTween.Hash (
+                "from", 0f,
+                "to",  1.0f,
+                "time", 1.0,
+                "onUpdate", "UpdateScoreDisplay"
+            ));
+        } else if (Input.GetMouseButtonUp(0)) {
+            iTween.Stop(gameObject);
+        }
+	}
+
+    void UpdateScoreDisplay(float newScore){
+        Debug.Log (newScore);
+        textMesh.text = newScore.ToString();
+    }
+}
---
+++ Assets/Scripts/Live2D/CalcParameter.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CalcParameter {
+
+    float targetParam;
+    float maxVelocity;
+
+     public CalcParameter()
+    {
+        targetParam = 0;
+        maxVelocity = 0;
+    }
+
+    public CalcParameter(float t, float v)
+    {
+        targetParam = t;
+        maxVelocity = v;
+    }
+
+
+}
